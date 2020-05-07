c4acf22fdd6701074a1626f59e964e66ec301af1
Ahmad Lufi A U <ahmad@nextbase.co>
2020-05-07 23:18:19 +0700

    connect mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+require('dotenv').config();
 
 const app = express()
+
+// connect to mlab database
+var mongodb_url = process.env.MONGOLAB_URL;
+mongoose
+ .connect(
+  mongodb_url,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+ )
+ .then(() => console.log("Connected to MongoDB Atlas"))
+ .catch(err => console.log("Error: ", err.message));
 
 app.use('/graphql', graphqlHTTP ({
     schema,
